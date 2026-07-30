795982dd9ebbea456cf8a1aa7c242fd6e5a736a3
ali-fr-2 <alifarjamy2@gmail.com>
2026-07-30 19:56:16 +0330

    231-yield-section

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,28 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>document</title>
-</head>
-
-<body>
-
-<header>
-    <p>my header</p>
-</header>
-
-<div>
-    <h1>contact page</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, id. Quod quasi, eligendi cumque odit, veniam sapiente ut necessitatibus tempore, debitis repellat reprehenderit? Iusto eligendi voluptas necessitatibus consequuntur officiis aut veniam maiores, consequatur odio. Voluptates sunt facere omnis voluptatibus illo at, deserunt necessitatibus reiciendis voluptatum tempore inventore quae. Ullam, fugiat!</p>
-</div>
-
-<footer>
-    <p>my footer</p>
-</footer>
-
-
-</body>
-
-</html>
+@extends('layout.master')
+@section('title')
+contact
+@endsection
+@section('body')
+    <div>
+        <h1>contact page</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, id. Quod quasi, eligendi cumque odit, veniam
+            sapiente ut necessitatibus tempore, debitis repellat reprehenderit? Iusto eligendi voluptas necessitatibus
+            consequuntur officiis aut veniam maiores, consequatur odio. Voluptates sunt facere omnis voluptatibus illo at,
+            deserunt necessitatibus reiciendis voluptatum tempore inventore quae. Ullam, fugiat!</p>
+    </div>
+@endsection
+@section('test')
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, tenetur.</p>
+@endsection
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+</head>
+
+<body>
+
+<header>
+    <p>my header</p>
+</header>
+
+@yield('body')
+
+<footer>
+    <p>my footer</p>
+</footer>
+
+@yield('test')
+</body>
+
+</html>
