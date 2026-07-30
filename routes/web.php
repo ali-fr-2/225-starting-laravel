6fdf5daa9d59d41eb5cd9f9310e0d4b6e3e9bc52
ali-fr-2 <alifarjamy2@gmail.com>
2026-07-30 20:24:47 +0330

    233-include-theme

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::get('/main', function () {
-//     return view('main', ['users' => [1, 2, 3]]);
-// });
-Route::get('/main', function () {
-    return view('main');
+Route::get('/index', function () {
+    return view('index');
 });
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/contact', function () {
-    return view('contact');
-});
+
