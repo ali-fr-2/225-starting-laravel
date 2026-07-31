7e661723cd5cacc17bca4b264c571c1aac3db751
ali-fr-2 <alifarjamy2@gmail.com>
2026-07-31 23:34:41 +0330

    240-route-name

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,16 @@
-@extends('layout.master')
+{{-- @extends('layout.master') --}}
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
 
+<h1>index</h1>
+<a href="{{ route('about') }}">about</a>
+<a href="{{ route('course',['course'=>'php','id'=>'100']) }}">coure</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
 Route::get('/welcome', function () {
     return view('welcome');
 });
-Route::get('courses/{course?}/{id?}', function ($course="python",$id=1) {
-    return $course." ".$id;
-});
+Route::get('courses/{course?}/{id?}', function ($course = "python", $id = 1) {
+    // return view('course');
+    return $course . " " . $id;
+})->name('course');
