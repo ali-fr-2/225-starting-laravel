a65cf0438428b1df9cdecc5aa239d1af5475ec11
ali-fr-2 <alifarjamy2@gmail.com>
2026-08-03 20:26:38 +0330

    245-crud-laravel

--- app/Http/Controllers/MyHome.php
+++ app/Http/Controllers/MyHome.php
@@ -15,7 +15,33 @@ class MyHome extends Controller
 
         // $courses=DB::table('courses')->where('id',2)->get();
 
-        $courses=DB::table('courses')->where('id','<',2)->get();
-        return view('index',compact('courses'));
+        // $courses=DB::table('courses')->where('id','<',2)->get();
+        // return view('index',compact('courses'));
+
+
+
+        // $courses=DB::table('courses')->insert([
+        //     ['title'=>'react','body'=>'test','user_name'=>'ali'],
+        //     ['title'=>'next.js','body'=>'test2','user_name'=>'reza']
+        // ]);
+
+        // $courses=DB::table('courses')->where('id',5)->update(['user_name'=>'asghar']);
+
+        // $courses=DB::table('courses')->where('id',5)->delete();
+
+        // $courses=DB::table('courses')->delete();
+
+
+
+
+        // if we use delete and again insert data the id will start from the last item that we have from before deleting but if we use truncate() it will start over from id 1
+
+
+
+        // $courses=DB::table('courses')->truncate();
+
+        // dd($courses);
+        return view('index');
+
     }
 }
