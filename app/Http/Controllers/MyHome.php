7e792777584e6bad51730c703b8a28d9cd70a865
ali-fr-2 <alifarjamy2@gmail.com>
2026-08-05 20:50:11 +0330

    255-global-scopes

--- app/Http/Controllers/MyHome.php
+++ app/Http/Controllers/MyHome.php
@@ -10,76 +10,15 @@ class MyHome extends Controller
 {
     public function index()
     {
-                                                        //insert
 
-        // $articles= new Articles();
-        // $articles->title="php";
-        // $articles->price="450000";
-        // $articles->body="this is for test";
-        // $articles->save();
+        // $articles=Articles::all();
         // dd($articles);
 
-                                                         //update
-
-        // $articles=Articles::find(3)->update([
-        //       'title'=>'python',
-        //       'price'=>'500000',
-        //       'body'=>'update testing'
-        // ]);
+        // $articles=Articles::all()->where('id','>',6);
         // dd($articles);
 
-        // $articles = Articles::find(3);
-        // $articles->update([
-        //     'title' => 'python',
-        //     'price' => '5000000',
-        //     'body' => 'update testing'
-        // ]);
-        // dd($articles);
-
-        // $articles=Articles::updateOrCreate(['id'=>8],[
-        //     'title'=>'c++',
-        //     'price'=>'50000',
-        //     'body'=>'testing'
-        // ]);
-        // dd($articles);
-
-                                                        // delete
-
-        // $articles=Articles::where('id',1)->delete();
-        // dd($articles);
-
-        // $articles=Articles::find(2)->delete();
-        // dd($articles);
-
-        // $articles = Articles::find(2);
-        // $articles->delete();
-        // dd($articles);
-
-        // $articles=Articles::destroy(3);
-        // dd($articles);
-
-        // $articles = Articles::destroy([3,4,5]);
-        // dd($articles);
-
-                                                        //soft-delete
-
-        // $articles=Articles::where('id',7)->delete();
-        // dd($articles);
-
-        // $articles=Articles::find(7);
-        // dd($articles);
-
-        // $articles=Articles::withTrashed()->find(7);
-        // dd($articles);
-
-        // $articles=Articles::onlyTrashed()->get();
-        // dd($articles);
-
-        // $articles=Articles::onlyTrashed()->find(7);
-        // $articles->restore();
-        // dd($articles);
-
-        $articles=Articles::where('id',7)->forceDelete();
+        $articles=Articles::all();
+        $articles=Articles::withoutGlobalScope('price')->where('id','>',6)->get();
         dd($articles);
 
     }
--- app/Models/Articles.php
+++ app/Models/Articles.php
@@ -4,14 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Override;
 
 class Articles extends Model
 {
     // protected $table="userpart";
 
-    protected $fillable = ['title','price','body'];
+    protected $fillable = ['title', 'price', 'body'];
 
     use SoftDeletes;
 
+    #[Override]
+    protected static function boot()
+    {
+        parent::boot();
 
+        static::addGlobalScope('price', function ($query) {
+            $query->where('price', '>', '100');
+        });
+    }
 }
