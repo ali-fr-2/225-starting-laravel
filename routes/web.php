9a68c6480a493b8cb71e655db5c4a6d7aee61cc8
ali-fr-2 <alifarjamy2@gmail.com>
2026-08-09 16:47:54 +0330

    257-form-in-laravel

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MyHome;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
-Route::get('/',[MyHome::class,'index']);
+// Route::get('/',[MyHome::class,'index']);
+
+Route::get('/login',[HomeController::class,'login'])->name('NameLogin');
+Route::post('/form',[HomeController::class,'form'])->name('NameForm');
 
 
 // Route::get('/', function () {
