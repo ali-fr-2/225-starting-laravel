072c3dcecf38af40257eac99845a6f5d9753e555
ali-fr-2 <alifarjamy2@gmail.com>
2026-08-09 16:57:56 +0330

    258-request-part-1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,16 @@ class HomeController extends Controller
     }
 
     public function form(Request $request){
-        dd($request->name);
+        // dd($request->name);
+        // dd($request->all());
+        // dd($request->email);
+        // dd($request->has('name'));
+        // dd($request->only('_token'));
+        dd($request->except('name'));
+
+
+
+
+
     }
 }
